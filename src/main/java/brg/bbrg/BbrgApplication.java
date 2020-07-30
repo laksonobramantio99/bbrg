0203d8b1152d8cca7ed8c92738471ec5602e53de
Laksono Bramantio <laxbramantio@gmail.com>
2020-07-30 21:11:16 +0700

    Initial commit

---
+++ src/main/java/brg/bbrg/BbrgApplication.java
@@ -0,0 +1,13 @@
+package brg.bbrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brg/bbrg/BbrgApplicationTests.java
@@ -0,0 +1,13 @@
+package brg.bbrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
