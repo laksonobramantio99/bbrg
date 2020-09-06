e0c7957f513c8de44d2b0c683a6cf5db76bddbbd
Laksono Bramantio <laxbramantio@gmail.com>
2020-09-07 00:02:45 +0700

    feat: Finish edit static page

--- src/main/java/brg/bbrg/controller/AdminController.java
+++ src/main/java/brg/bbrg/controller/AdminController.java
@@ -1,12 +1,22 @@
 package brg.bbrg.controller;
 
+import brg.bbrg.model.StaticPageModel;
+import brg.bbrg.service.StaticPageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private StaticPageService staticPageService;
 
     @GetMapping(value = "")
     public String adminHome() {
@@ -14,7 +24,16 @@ public class AdminController {
     }
 
     @GetMapping(value = "/static-page")
-    public String staticPage() {
-        return "static-page-manage";
+    public String staticPage(Model model) {
+        List<StaticPageModel> staticPageModelList = staticPageService.getAll();
+        model.addAttribute("staticPageModelList", staticPageModelList);
+        return "admin-staticpage-manage";
+    }
+
+    @GetMapping(value = "/static-page/edit")
+    public String editStaticPage(@RequestParam Long id, Model model) {
+        StaticPageModel staticPageModel = staticPageService.getById(id);
+        model.addAttribute("staticPageModel", staticPageModel);
+        return "admin-staticpage-edit";
     }
 }
